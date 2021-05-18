a550196b24f5e6703317bab7a76292199e34a719
Daniella Marks <marks.daniella@gmail.com>
2021-05-18 16:18:44 -0700

    second test passes

--- src/code-challenge-14.js
+++ src/code-challenge-14.js
@@ -1,8 +1,18 @@
 export function replaceZeroes(string){
   //<solution>
-  const result = string.replace(/0/gm, `zero`);
+  const result = string.replace(/0/g, `zero`);
   console.log(result);
 
   return result;
+
+};
+
+export function validatePin(pin) {
+
+  //<solution>
+  if (pin.match(/\d{4}$/gm)) {
+    return true;
+  } else { return false;
+  }
 
 };
